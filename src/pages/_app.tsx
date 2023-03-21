af12d4e6f317cd37d9911ec5cbd8b7b2cfab6335
doganeraylin <doganeraylinn@gmail.com>
2023-03-21 16:15:10 +0200

    added apolloprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client';
+import useApollo from '../../hooks/useApollo';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = useApollo(pageProps);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
