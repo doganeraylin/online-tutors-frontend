d04a6a5457a9fe0dd9b2a8534c0925d0b08853a8
doganeraylin <doganeraylinn@gmail.com>
2023-03-21 16:14:25 +0200

    setting up apollo

---
+++ apollo/index.ts
@@ -0,0 +1,58 @@
+import merge from 'deepmerge';
+import isEqual from 'lodash-es/isEqual';
+import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+
+
+const BIRDS_API = 'http://localhost:1337/graphql';
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | null;
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    uri: BIRDS_API,
+    cache: new InMemoryCache(),
+  });
+}
+
+export function initializeApollo(initialState?: any) {
+    const _apolloClient = apolloClient ?? createApolloClient();
+  
+    if (initialState) {
+      const existingCache = _apolloClient.cache.extract();
+  
+      const data = merge(initialState, existingCache, {
+        arrayMerge: (destinationArray, sourceArray) => [
+          ...sourceArray,
+          ...destinationArray.filter((d) =>
+            sourceArray.every((s) => !isEqual(d, s))
+          ),
+        ],
+      });
+      _apolloClient.cache.restore(data);
+    }
+  
+    if (typeof window === 'undefined') {
+      return _apolloClient;
+    }
+  
+    if (!apolloClient) {
+      apolloClient = _apolloClient;
+    }
+  
+    return _apolloClient;
+  }
+
+
+//   
+export function addApolloState(
+    client: ApolloClient<NormalizedCacheObject>,
+    pageProps: any
+  ) {
+    if (pageProps?.props) {
+      pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+    }
+  
+    return pageProps;
+  }
