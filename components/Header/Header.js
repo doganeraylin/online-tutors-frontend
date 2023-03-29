222d18cec745415cc8820ee5984d89cf8f38727c
doganeraylin <doganeraylinn@gmail.com>
2023-03-29 11:39:54 +0200

    edited classNames and added bootstrap classes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from "./Header.module.scss"
+import styles from "./Header.module.css"
 
 
 
@@ -12,34 +12,46 @@ const Header = () => {
         { href: '/contact', text: 'contact' }
       ];
 
-    
-    return (
-    <>
-    <div className={styles.headerContainer}>
-         <div className={styles.logoImgContainer}>
-            <img className={styles.logoImg} src="./assets/header/flower-logo.png"></img>
-            <div className={styles.logoTextContainer}>
-                 <p className={styles.logoTextTop}>playful</p>
-                 <p className={styles.logoTextBottom}>minds!</p>
-            </div>
-         </div>
-       
-         <div className={styles.navbarContainer}>
-            {links.map((link, index) => (
-                <div key={index}>
-                    <Link className={styles.navLinks} href={link.href}>{link.text}</Link>
-                    {index < links.length - 1 && <img className={styles.dotImg} src="./assets/header/header-dot.png"></img>}
-                </div>
-            ))}
-         </div>
-         <div className={styles.loginContainer}>
-            <Link href="" ><button className={styles.loginBtn}></button></Link>
-         </div>
-    </div>
-</>
+      return (
+        <nav className={`${styles.navbarContainer} navbar navbar-expand-md`}>
+          <div className="container-fluid d-flex align-items-start align-items-md-center">
+            {/*  */}
+            {/* <div className={`${styles.logoContainer}`}>
+              <Link href="/" legacyBehavior>
+                <img className={`${styles.navbarLogo} w-25`}src="./assets/header/logo.png" alt="logo-img"></img>
+              </Link>
+            </div> */}
+            {/*  */}
 
-    )
+              <Link href="/" legacyBehavior>
+                <img className={`${styles.navbarLogo} w-25`}src="./assets/header/logo.png" alt="logo-img"></img>
+              </Link>
+         
+            <button className={`${styles.navbarToggler} navbar-toggler d-md-none mt-1`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className={`${styles.navbarToggleIcon} navbar-toggler-icon `} ></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className={`${styles.navLinks} navbar-nav mb-2 mb-md-0 `} style={{ '--bs-scroll-height': '180px' }}>
+              {links.map((link, index) => (
+               <div key={index}>
+                 <Link className="text-decoration-none px-md-2" href={link.href}>{link.text}</Link>
+                {index < links.length - 1 && <img className={`${styles.dotImg} d-none d-md-inline`}src="./assets/header/header-dot.png"></img>}
+               </div>
+              ))}
+              </ul>
+            </div>
+            <div className={`${styles.btnContainer} d-none d-md-block`}>
+              <button type="button" className={styles.loginBtn}>Login</button>
+              {/* <button className={`${styles.loginBtn} px-3 py-1`}>Login</button> */}
+            </div>
+          </div>
+        </nav>
+      ) 
+
+ 
+  
 }
 
-
 export default Header 
+
+
