85b91feb609dfd7452c3416a7be52b47837d9d54
doganeraylin <doganeraylinn@gmail.com>
2023-03-29 11:40:41 +0200

    added bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import '../../styles/globals.scss'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client';
 import useApollo from '../../hooks/useApollo';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   const client = useApollo(pageProps);
   return (
     <ApolloProvider client={client}>
