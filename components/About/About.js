844b68eeeaa050bd7665fc7e3cf70fd05754227b
doganeraylin <doganeraylinn@gmail.com>
2023-04-28 08:01:52 +0200

    removed a section

--- components/About/About.js
+++ components/About/About.js
@@ -41,7 +41,7 @@ const About = () => {
             {/*  */}
             <div className={styles.featureContainer}>
                 <div className={styles.featureTitleContainer}>
-                    <p className={styles.featureTwoTitle}>Personalized Learning at its Best.</p>
+                    <p className={styles.featureTitleCentered}>Personalized Learning at its Best.</p>
                 </div>
                 <div className={styles.featureDescriptionContainer}>
                     <p className={styles.featureDescription}>Our core service is personalized <span>one-on-one</span> sessions with <span>expert tutors</span> who work closely with your child to develop a <span>customized learning plan</span> that meets their individual needs.</p>
@@ -57,15 +57,48 @@ const About = () => {
                         <img className={styles.codingImgOne} src="./assets/about/arrow.png"></img>
                         <img className={styles.codingImgTwo} src="./assets/about/smiley-face.png"></img>
                     </div>
-                    {/* <div className={styles.singleSubjectContainer}>
-                        <p className={`${styles.subject} ${styles.science}`}>science</p>
-                        <img src="./assets/about/cloud.png"></img>
-                        <img src="./assets/about/flower.png"></img>
-                    </div> */}
                     <div className={styles.singleSubjectContainer}>
                         <p className={`${styles.subject} ${styles.language}`}>language</p>
                         <img className={styles.languageImg} src="./assets/about/ambitious.png"></img>
                     </div>                 
+                </div>
+            </div>
+
+            {/*  */}
+            <div className={styles.featureContainer}>
+                <div clasName={styles.featureTitleContainer}>
+                    <p className={styles.featureTitleCentered}>Fun and Interactive Learning Resources</p>
+                </div>
+                <div className={styles.featureDescriptionContainer}>
+                    <p className={styles.featureDescription}>We offer a variety of learning resources and tools to help children learn and grow. 
+                    These include...</p>
+                </div>
+                <div className={styles.materialsContainer}>
+                    <div className={styles.singleMaterialContainer}>
+                        <p className={`${styles.material} ${styles.games}`}>games</p>
+                        <img className={styles.gamesImg} src="./assets/about/happy-boy.png"></img>
+                    </div>
+                    <div className={styles.singleMaterialContainer}>
+                        <p className={`${styles.material} ${styles.quizzes}`}>quizzes</p>
+                        <img className={styles.quizzesImgOne} src="./assets/about/cloud.png"></img>
+                        <img className={styles.quizzesImgTwo} src="./assets/about/arrow.png"></img>
+                    </div>
+                    <div className={styles.singleMaterialContainer}>
+                        <p className={`${styles.material} ${styles.ebooks}`}>e-books</p>
+                        <img className={styles.ebooksImg} src="./assets/about/ambitious.png"></img>
+                    </div>                 
+                </div>
+            </div>
+            <div className={styles.featureContainer}>
+                <div className={styles.summaryContainer}>
+                    <p>Overall, our goal at Playful Minds is to provide a safe and supportive learning environment that fosters creativity and learning.</p>
+                    <p>We believe that every child has the potential to achieve great things, and we're committed to helping them unlock their full potential through fun and engaging learning experiences.</p>
+                </div>
+                <div className={styles.ctaTextContainer}>
+                    <p className={styles.ctaText}>embark on an exciting learning adventure today!</p>
+                </div>
+                <div className={styles.btn}>
+                    <button>sign up</button>
                 </div>
             </div>
         </div>
