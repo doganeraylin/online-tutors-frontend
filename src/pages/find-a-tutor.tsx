b3f3402b7b300ff842453cdbb8c356048aa990e8
doganeraylin <doganeraylinn@gmail.com>
2023-05-02 09:21:10 +0200

    created pages for navs

---
+++ src/pages/find-a-tutor.tsx
@@ -0,0 +1,22 @@
+import TutorCard from '../../components/TutorCard/TutorCard'
+import TutorReview from '../../components/TutorReviews/TutorReviews'
+import Filter from '../../components/Filter/Filter'
+
+
+const findATutor = () => 
+<>
+<Filter/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+<TutorCard/>
+
+</>
+
+
+export default findATutor
