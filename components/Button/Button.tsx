8381284f691375e88e71ab20418e4636736f2f29
doganeraylin <doganeraylinn@gmail.com>
2023-05-02 09:17:47 +0200

    created button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import styles from './Button.module.css'
+
+interface ButtonProps {
+    content: string
+    buttonColor: "orangeBg" | "lightYellowBg" | "grayBg"
+    buttonTextColor: "whiteText" | "blueText" | "grayText" | "orangeText"
+    buttonSize: "smallBtn" | "mediumBtn" | "largeBtn" | "largestSize"
+    buttonFontSize: "smallFont" | "mediumFont" | "largeFont"
+    height: "smallHeight" | "mediumHeight" | "largeHeight"
+
+    onClick?: () => void
+  }
+  
+  const Button: React.FC<ButtonProps> = 
+  ({ content,
+     buttonColor, 
+     buttonTextColor, 
+     buttonSize, 
+     buttonFontSize, 
+     height, 
+     onClick }) => {
+
+    const handleClick = () => {
+      if (onClick) {
+        onClick()
+      }
+    }
+  
+    return (
+      <button 
+        className={`${styles[buttonColor]} 
+                  ${styles[buttonTextColor]} 
+                  ${styles[buttonSize]} 
+                  ${styles[buttonFontSize]}
+                  ${styles[height]}
+                  ${styles.commonStyle}`}
+                  onClick={handleClick}>
+                  {content}
+      </button>
+    )
+  }
+  
+  export default Button
+  
