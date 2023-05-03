fbe74adcb62215d87e2bbbed5e1d80706422417a
doganeraylin <doganeraylinn@gmail.com>
2023-05-03 10:21:35 +0200

    edited class names

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,8 +3,8 @@ import Button from '../Button/Button'
 import styles from './Hero.module.css'
 
 const Hero = () => (
-    <div className={`${styles.heroContainer} d-flex flex-column flex-md-row align-items-lg-center`}>
-        <div className={`${styles.heroTextContainer} w-100 me-4`}>
+    <div className={`${styles.heroContainer}`}>
+        <div className={`${styles.heroTextContainer} w-100`}>
             <h1 className={styles.heroTextTitle}>Playful Learning Adventures</h1>
             <h2 className={styles.heroTextSubtitle}>Transform your child's learning experience with Playful Minds!</h2>
             <p className={styles.heroTextDescription}>Unlock your child's full potential with our <span>fun</span> and  <span>engaging</span> online tutoring program. Our <span>expert tutors</span> provide personalized <span>one-on-one sessions</span> that cater to your child's <span>unique learning style</span>. Sign up today and watch your child's mind soar!</p>
@@ -17,14 +17,12 @@ const Hero = () => (
                         buttonSize="largeBtn"
                         buttonFontSize="largeFont"
                         height="largeHeight"
-                      
-            
                     />
                 </Link>
             </div>
         </div>
         <div className={styles.heroImgContainer}>
-            <img className={`${styles.heroImg} w-100 d-none d-lg-block`}src="./assets/hero/hero-img.png"></img>
+            <img className={`${styles.heroImg} w-100 `}src="./assets/hero/hero-img.png"></img>
         </div>
     </div>
 )
