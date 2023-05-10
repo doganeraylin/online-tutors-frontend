2de9603e0fb2fe776eda551599954707d212e01e
doganeraylin <doganeraylinn@gmail.com>
2023-05-10 11:48:35 +0200

    created register component

---
+++ components/Register/Register.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link"
+import Header from "../Header/Header"
+import Button from "../Button/Button"
+import styles from "./Register.module.css"
+
+const Register = () => {
+    return (
+    <>
+        <Header/>
+        <div className={styles.container}>
+            <div className={styles.form}>
+                <div className={styles.imgContainer}>
+                    <img className={styles.pencil} src="./assets/login/astronaut.png"></img>
+                </div>
+                <div className={styles.formContent}>
+                <h1>Create an account</h1>
+                    <input type="text" placeholder="username"></input>
+                    <input type="email" placeholder="email"></input>
+                    <input type="password" placeholder="password"></input>
+                    <div className={styles.btnContainer}>
+                        <Link href="">
+                            <Button 
+                                content="register"
+                                buttonColor="orangeBg"
+                                buttonTextColor="whiteText"
+                                buttonSize="smallBtn"
+                                buttonFontSize="smallFont"
+                                height="smallHeight"
+                            />
+                        </Link>
+                    </div>
+             
+                </div>  
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Register
