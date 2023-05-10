f1b6a0f0fae4e79fe28bf5dc17fd6c520af37833
doganeraylin <doganeraylinn@gmail.com>
2023-05-10 11:55:48 +0200

    animated explore tutors elements

--- components/ExploreTutors/ExploreTutors.tsx
+++ components/ExploreTutors/ExploreTutors.tsx
@@ -1,6 +1,66 @@
+import Link from 'next/link'
+import Button from '../Button/Button'
+import { useRef, useEffect} from "react"
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 import styles from './ExploreTutors.module.css'
 
 const ExploreTutors = () => {
+
+    const starRef = useRef<HTMLDivElement>(null);
+    const starRef2 = useRef<HTMLDivElement>(null);
+    const starRef3 = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+    let ctx = gsap.context(() => {
+        gsap.to(starRef.current, {
+        duration: 4,
+        scale: 1.3,
+        // rotate: "+360",
+        ease: "slow(2, 2, false)",
+        repeat: -1,
+        yoyo: true,
+        scrollTrigger: {
+            trigger: starRef.current,
+            start: "top 100%",
+            end: "bottom top",
+        }
+        });
+        gsap.to(starRef2.current, {
+            duration: 4,
+            scale: 1.3,
+            // rotate: "+360",
+            ease: "slow(2, 2, false)",
+            repeat: -1,
+            yoyo: true,
+            scrollTrigger: {
+                trigger: starRef2.current,
+                start: "top 100%",
+                end: "bottom top",
+            }
+            });
+            gsap.to(starRef3.current, {
+                duration: 4,
+                scale: 1.3,
+                // rotate: "+360",
+                ease: "slow(2, 2, false)",
+                repeat: -1,
+                yoyo: true,
+                scrollTrigger: {
+                    trigger: starRef3.current,
+                    start: "top 100%",
+                    end: "bottom top",
+                }
+                });
+
+    }, [starRef, starRef2, starRef3]);
+    
+    return () => {
+        ctx.kill();
+    }
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.wavyLinesContainer}>
@@ -8,8 +68,11 @@ const ExploreTutors = () => {
                 <img src=""></img>
             </div>
             <div className={styles.tutorsContainer}>
-                <div className={`${styles.firstStar} ${styles.singleTutor}`}>
-                    <img className= {styles.star} src="./assets/exploreTutors/star-center.png"></img>
+                <div ref={starRef}  className={`${styles.firstStar} ${styles.singleTutor}`}>
+                    <div >
+                        <img className={styles.star} src="./assets/exploreTutors/star-center.png"></img>
+                    </div>
+                 
                     <div className={styles.tutorInfo}>
                         <div className={styles.tutorProfile}>
                             <img className={styles.tutorImg} src="./assets/contact/round-shape.png"></img>
@@ -19,14 +82,14 @@ const ExploreTutors = () => {
                             <img className={styles.ratingIcon} src="./assets/tutor/rating.png"></img>
                             <p className={styles.rating}>4.9</p>
                             <p className={styles.dotShape}></p>
-                            <p>232 reviews</p>
+                            <p className={styles.reviewCount}>232 reviews</p>
                         </div>
                         <div className={styles.tutorQuote}>
-                            <p> <p>"I'm here to help you learn and reach your full potential!"</p></p>
+                            <p>"I'm here to help you learn and reach your full potential!"</p>
                         </div>
                     </div>
                 </div>
-                <div className={`${styles.secondStar} ${styles.singleTutor}`}>
+                <div ref={starRef2} className={`${styles.secondStar} ${styles.singleTutor}`}>
                     <div>
                         <img className={`${styles.star}`} src="./assets/exploreTutors/star-center.png"></img>
                         <div className={styles.tutorInfo}>
@@ -41,12 +104,12 @@ const ExploreTutors = () => {
                                 <p>232 reviews</p>
                             </div>
                             <div className={styles.tutorQuote}>
-                                <p> <p>"I'm here to help you learn and reach your full potential!"</p></p>
+                                <p>"I'm here to help you learn and reach your full potential!"</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={`${styles.thirdStar} ${styles.singleTutor}`}>
+                <div ref={starRef3} className={`${styles.thirdStar} ${styles.singleTutor}`}>
                     <img className= {styles.star} src="./assets/exploreTutors/star-center.png"></img>
                     <div className={styles.tutorInfo}>
                         <div className={styles.tutorProfile}>
@@ -60,11 +123,22 @@ const ExploreTutors = () => {
                             <p>232 reviews</p>
                         </div>
                         <div className={styles.tutorQuote}>
-                            <p> <p>"I'm here to help you learn and reach your full potential!"</p></p>
+                            <p>"I'm here to help you learn and reach your full potential!"</p>
                         </div>
                     </div>
                 </div>
-                
+            </div>
+            <div className={styles.btnContainer}>
+                <Link href="">
+                    <Button 
+                        content="explore all tutors"
+                        buttonColor="orangeBg"
+                        buttonTextColor="whiteText"
+                        buttonSize="largeBtn"
+                        buttonFontSize="largeFont"
+                        height="largeHeight"
+                    />
+                </Link>
             </div>
         </div>
     )
