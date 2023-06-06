795749bfba03fb4aa486526970ba1024bcab91d4
doganeraylin <doganeraylinn@gmail.com>
2023-06-06 15:56:29 +0200

    added footer without birds

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import About from "../../components/About/About"
+import Footer from "../../components/Footer/Footer"
 import Layout from "../../components/Layout/Layout";
 
 const about = () => 
 <Layout bgColor=" #BAD0F6">
    <About/>
+   <Footer showImgContainer={false} />
 </Layout>
 
 export default about;
