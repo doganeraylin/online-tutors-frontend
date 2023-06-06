14642333380abb7bbea3b735f8a383ce14ade88e
doganeraylin <doganeraylinn@gmail.com>
2023-06-06 15:51:17 +0200

    edited animation / classnames

--- components/ExploreTutors/ExploreTutors.tsx
+++ components/ExploreTutors/ExploreTutors.tsx
@@ -1,95 +1,70 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import Button from '../Button/Button'
 import { useRef, useEffect} from "react"
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
+import { matchMedia, mediaQueries } from "../AnimationScreenSize/animationScreenSize"
 import styles from './ExploreTutors.module.css'
+gsap.registerPlugin(ScrollTrigger);
 
 const ExploreTutors = () => {
-
-    const starRef = useRef<HTMLDivElement>(null);
-    const starRef2 = useRef<HTMLDivElement>(null);
-    const starRef3 = useRef<HTMLDivElement>(null);
+    const router = useRouter()
+    const animationRef = useRef<HTMLDivElement>(null);
+    const handleBtnClick = () => router.push('/find-a-tutor')
 
     useEffect(() => {
-    let ctx = gsap.context(() => {
-        gsap.to(starRef.current, {
-        duration: 4,
-        scale: 1.3,
-        // rotate: "+360",
-        ease: "slow(2, 2, false)",
-        repeat: -1,
-        yoyo: true,
-        scrollTrigger: {
-            trigger: starRef.current,
-            start: "top 100%",
-            end: "bottom top",
-        }
-        });
-        gsap.to(starRef2.current, {
-            duration: 4,
-            scale: 1.3,
-            // rotate: "+360",
-            ease: "slow(2, 2, false)",
-            repeat: -1,
-            yoyo: true,
-            scrollTrigger: {
-                trigger: starRef2.current,
-                start: "top 100%",
-                end: "bottom top",
-            }
-            });
-            gsap.to(starRef3.current, {
-                duration: 4,
-                scale: 1.3,
-                // rotate: "+360",
-                ease: "slow(2, 2, false)",
-                repeat: -1,
-                yoyo: true,
-                scrollTrigger: {
-                    trigger: starRef3.current,
-                    start: "top 100%",
-                    end: "bottom top",
-                }
-                });
+        matchMedia.add(mediaQueries, (context) => {
+            let isDesktop = context.conditions?.isDesktop ?? false;
+            if (isDesktop) {
+                let ctx = gsap.context(() => {
+                    gsap.to(".first-star", {
+                        duration: 1.5,
+                        scale: 1.3,
+                        ease: "power4.out",
+                        yoyo: true,
+                        scrollTrigger: {
+                            trigger: ".first-star",
+                            start: "top 20%", 
+                            end: "bottom top",
+                            scrub: true,
+                        }
+                        });
+                    gsap.to(".second-star", {
+                        duration: 1.5,
+                        scale: 1.3,
+                        ease: "power4.out",
+                        yoyo: true,
+                        scrollTrigger: {
+                            trigger: ".second-star",
+                            start: "top 20%", 
+                            end: "bottom top",
+                            scrub: true,
+                        }
+                        });
+                        gsap.to(".third-star", {
+                            duration: 1.5,
+                            scale: 1.3,
+                            ease: "power4.out",
+                            yoyo: true,
+                            scrollTrigger: {
+                                trigger: ".third-star",
+                                start: "top 20%", 
+                                end: "bottom top",
+                                scrub: true,
+                            }
+                            });
+                }, [animationRef]);
 
-    }, [starRef, starRef2, starRef3]);
-    
-    return () => {
-        ctx.kill();
-    }
-    }, []);
+                return () => ctx.kill();
+            }   
+        })
+    }, [animationRef]);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.wavyLinesContainer}>
-                <img src=""></img>
-                <img src=""></img>
-            </div>
+        <div ref={animationRef} className={styles.container}>
             <div className={styles.tutorsContainer}>
-                <div ref={starRef}  className={`${styles.firstStar} ${styles.singleTutor}`}>
-                    <div >
-                        <img className={styles.star} src="./assets/exploreTutors/star-center.png"></img>
-                    </div>
-                 
-                    <div className={styles.tutorInfo}>
-                        <div className={styles.tutorProfile}>
-                            <img className={styles.tutorImg} src="./assets/contact/round-shape.png"></img>
-                            <p>Jack Sth</p>
-                        </div>
-                        <div className={styles.tutorRating}>
-                            <img className={styles.ratingIcon} src="./assets/tutor/rating.png"></img>
-                            <p className={styles.rating}>4.9</p>
-                            <p className={styles.dotShape}></p>
-                            <p className={styles.reviewCount}>232 reviews</p>
-                        </div>
-                        <div className={styles.tutorQuote}>
-                            <p>"I'm here to help you learn and reach your full potential!"</p>
-                        </div>
-                    </div>
-                </div>
-                <div ref={starRef2} className={`${styles.secondStar} ${styles.singleTutor}`}>
+                <div className={`${styles.thirdStar} ${styles.singleTutor} first-star`}>
                     <div>
                         <img className={`${styles.star}`} src="./assets/exploreTutors/star-center.png"></img>
                         <div className={styles.tutorInfo}>
@@ -109,7 +84,27 @@ const ExploreTutors = () => {
                         </div>
                     </div>
                 </div>
-                <div ref={starRef3} className={`${styles.thirdStar} ${styles.singleTutor}`}>
+                <div className={`${styles.secondStar} ${styles.singleTutor} second-star`}>
+                    <div>
+                        <img className={`${styles.star}`} src="./assets/exploreTutors/star-center.png"></img>
+                        <div className={styles.tutorInfo}>
+                            <div className={styles.tutorProfile}>
+                                <img className={styles.tutorImg} src="./assets/contact/round-shape.png"></img>
+                                <p>Jack Sth</p>
+                            </div>
+                            <div className={styles.tutorRating}>
+                                <img className={styles.ratingIcon} src="./assets/tutor/rating.png"></img>
+                                <p className={styles.rating}>4.9</p>
+                                <p className={styles.dotShape}></p>
+                                <p>232 reviews</p>
+                            </div>
+                            <div className={styles.tutorQuote}>
+                                <p>"I'm here to help you learn and reach your full potential!"</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${styles.thirdStar} ${styles.singleTutor} third-star`}>
                     <img className= {styles.star} src="./assets/exploreTutors/star-center.png"></img>
                     <div className={styles.tutorInfo}>
                         <div className={styles.tutorProfile}>
@@ -137,6 +132,7 @@ const ExploreTutors = () => {
                         buttonSize="largeBtn"
                         buttonFontSize="largeFont"
                         height="largeHeight"
+                        onClick={handleBtnClick}
                     />
                 </Link>
             </div>
