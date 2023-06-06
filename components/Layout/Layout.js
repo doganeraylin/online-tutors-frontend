09c24b8f37e599821774a96601f27a7ab02f863e
doganeraylin <doganeraylinn@gmail.com>
2023-06-06 15:53:14 +0200

    added footer component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
 
 const Layout = ({ children, bgColor }) => (
   <div style={{ backgroundColor: bgColor }}>
     <Header />
     {children}
+    <Footer/>
   </div>
 )
 
