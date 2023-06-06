a5da7a8effa685334f5f7afe3ff43653fa62ef90
doganeraylin <doganeraylinn@gmail.com>
2023-06-06 15:45:34 +0200

    added layout

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,9 @@
+import Layout from "../../components/Layout/Layout";
 import Success from "../../components/Success/Success"
 
-const success = () => <Success/>
+const success = () => 
+<Layout bgColor="#BAD0F6">
+    <Success/>
+</Layout>
 
 export default success
