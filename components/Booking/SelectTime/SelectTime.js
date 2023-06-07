24247a52c9cda158478577f56b0fcb8e56987bf6
doganeraylin <doganeraylinn@gmail.com>
2023-06-07 10:43:39 +0200

    added fullcalendar

--- components/Booking/SelectTime/SelectTime.js
+++ components/Booking/SelectTime/SelectTime.js
@@ -1,24 +1,26 @@
-const SelectTime = ({ onSelect }) => {
-    const times = [30, 60, 90, 120]; // Example array of available times
-  
-    const handleTimeSelect = (time) => {
-      onSelect(time);
-    };
-  
-    return (
-      <div>
-        <h2>Select a Time</h2>
-        <ul>
-          {times.map((time, index) => (
-            <li key={index}>
-              <p>{time} minutes</p>
-              <button onClick={() => handleTimeSelect(time)}>Select</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
-  
-  export default SelectTime;
-  
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import styles from "./SelectTime.module.css" 
+
+const SelectTime = () => {
+  return (
+    <div className={styles.fullCalendar}>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        selectable={true}
+        slotMinTime="09:00:00"
+        slotDuration="00:30:00" 
+        eventTextColor = "black"
+        eventColor= "#378006"
+      />
+    </div>
+  );
+};
+
+export default SelectTime;
+
+
