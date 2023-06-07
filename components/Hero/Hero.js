d426c93e580e3f554afb92b5cb62cf35d27e510f
doganeraylin <doganeraylinn@gmail.com>
2023-06-07 10:45:44 +0200

    removed useRouter

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,14 +1,9 @@
 import Link from 'next/link'
-import { Router, useRouter } from 'next/router'
 import Button from '../Button/Button'
 import styles from './Hero.module.css'
 
 
 const Hero = () => { 
-
-    const router = useRouter()
-
-    const handleBtnClick = () => router.push('/register')
 
     return (
         <div className={`${styles.container}`}>
@@ -17,7 +12,7 @@ const Hero = () => {
                 <h2 className={styles.heroTextSubtitle}>Transform your child's learning experience with Playful Minds!</h2>
                 <p className={styles.heroTextDescription}>Unlock your child's full potential with our <span>fun</span> and  <span>engaging</span> online tutoring program. Our <span>expert tutors</span> provide personalized <span>one-on-one sessions</span> that cater to your child's <span>unique learning style</span>. Sign up today and watch your child's mind soar!</p>
                 <div className={styles.heroBtnContainer}>
-                    <Link href="">
+                    <Link href="/register">
                         <Button 
                             content="get started"
                             buttonColor="orangeBg"
@@ -25,7 +20,7 @@ const Hero = () => {
                             buttonSize="largeBtn"
                             buttonFontSize="largeFont"
                             height="largeHeight"
-                            onClick={handleBtnClick}
+    
                         />
                     </Link>
                 </div>
