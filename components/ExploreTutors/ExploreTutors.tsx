7223ccf62244e5724260c3ec0cdcbf33803820b9
doganeraylin <doganeraylinn@gmail.com>
2023-06-07 10:44:57 +0200

    removed useRouter / edited tutor texts

--- components/ExploreTutors/ExploreTutors.tsx
+++ components/ExploreTutors/ExploreTutors.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router';
 import Button from '../Button/Button'
 import { useRef, useEffect} from "react"
 import { gsap } from 'gsap';
@@ -9,9 +8,7 @@ import styles from './ExploreTutors.module.css'
 gsap.registerPlugin(ScrollTrigger);
 
 const ExploreTutors = () => {
-    const router = useRouter()
     const animationRef = useRef<HTMLDivElement>(null);
-    const handleBtnClick = () => router.push('/find-a-tutor')
 
     useEffect(() => {
         matchMedia.add(mediaQueries, (context) => {
@@ -69,17 +66,17 @@ const ExploreTutors = () => {
                         <img className={`${styles.star}`} src="./assets/exploreTutors/star-center.png"></img>
                         <div className={styles.tutorInfo}>
                             <div className={styles.tutorProfile}>
-                                <img className={styles.tutorImg} src="./assets/contact/round-shape.png"></img>
-                                <p>Jack Sth</p>
+                                <img className={styles.tutorImg} src="./assets/tutors/emma.png"></img>
+                                <p>Emma S.</p>
                             </div>
                             <div className={styles.tutorRating}>
                                 <img className={styles.ratingIcon} src="./assets/tutor/rating.png"></img>
-                                <p className={styles.rating}>4.9</p>
+                                <p className={styles.rating}>4.5</p>
                                 <p className={styles.dotShape}></p>
-                                <p>232 reviews</p>
+                                <p>50 lessons</p>
                             </div>
                             <div className={styles.tutorQuote}>
-                                <p>"I'm here to help you learn and reach your full potential!"</p>
+                                <p>"I'm here to help reach your full potential!"</p>
                             </div>
                         </div>
                     </div>
@@ -89,17 +86,17 @@ const ExploreTutors = () => {
                         <img className={`${styles.star}`} src="./assets/exploreTutors/star-center.png"></img>
                         <div className={styles.tutorInfo}>
                             <div className={styles.tutorProfile}>
-                                <img className={styles.tutorImg} src="./assets/contact/round-shape.png"></img>
-                                <p>Jack Sth</p>
+                                <img className={styles.tutorImg} src="./assets/tutors/liam.png"></img>
+                                <p>Liam K.</p>
                             </div>
                             <div className={styles.tutorRating}>
                                 <img className={styles.ratingIcon} src="./assets/tutor/rating.png"></img>
-                                <p className={styles.rating}>4.9</p>
+                                <p className={styles.rating}>4.8</p>
                                 <p className={styles.dotShape}></p>
-                                <p>232 reviews</p>
+                                <p>75 lessons</p>
                             </div>
                             <div className={styles.tutorQuote}>
-                                <p>"I'm here to help you learn and reach your full potential!"</p>
+                                <p>"I have a passion for helping students"</p>
                             </div>
                         </div>
                     </div>
@@ -108,23 +105,23 @@ const ExploreTutors = () => {
                     <img className= {styles.star} src="./assets/exploreTutors/star-center.png"></img>
                     <div className={styles.tutorInfo}>
                         <div className={styles.tutorProfile}>
-                            <img className={styles.tutorImg} src="./assets/contact/round-shape.png"></img>
-                            <p>Jack Sth</p>
+                            <img className={styles.tutorImg} src="./assets/tutors/ava.png"></img>
+                            <p>Ava S.</p>
                         </div>
                         <div className={styles.tutorRating}>
                             <img className={styles.ratingIcon} src="./assets/tutor/rating.png"></img>
-                            <p className={styles.rating}>4.9</p>
+                            <p className={styles.rating}>4.2</p>
                             <p className={styles.dotShape}></p>
-                            <p>232 reviews</p>
+                            <p>40 reviews</p>
                         </div>
                         <div className={styles.tutorQuote}>
-                            <p>"I'm here to help you learn and reach your full potential!"</p>
+                            <p>"I love sharing my language with others."</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={styles.btnContainer}>
-                <Link href="">
+                <Link href="/find-a-tutor">
                     <Button 
                         content="explore all tutors"
                         buttonColor="orangeBg"
@@ -132,7 +129,7 @@ const ExploreTutors = () => {
                         buttonSize="largeBtn"
                         buttonFontSize="largeFont"
                         height="largeHeight"
-                        onClick={handleBtnClick}
+
                     />
                 </Link>
             </div>
