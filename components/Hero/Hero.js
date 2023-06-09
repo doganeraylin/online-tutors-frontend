46e080b5cb9822fd8ebc0b4fdae88a3c28ca14c5
doganeraylin <doganeraylinn@gmail.com>
2023-06-09 14:22:41 +0200

    added arrow img and animated it

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,28 +1,45 @@
 import Link from 'next/link'
-import Button from '../Button/Button'
+import { useRef, useEffect} from "react"
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { matchMedia, mediaQueries } from "../AnimationScreenSize/animationScreenSize"
 import styles from './Hero.module.css'
-
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => { 
-
+    const arrowRef = useRef(null);
+    const animationTriggerRef = useRef(null)
+    useEffect(() => {
+        matchMedia.add(mediaQueries, (context) => {
+            let isDesktop = context.conditions?.isDesktop ?? false;
+            if(isDesktop) {
+                let ctx = gsap.context(() => {
+                    gsap.to(arrowRef.current, {
+                        duration: 2.5,
+                        y: 70,
+                        ease: "power1.out",
+                        repeat: -1,
+                        yoyo: true,
+                        scrollTrigger: {
+                            trigger: arrowRef.current,
+                            start: "top 90%",
+                        }
+                    });
+                }, [arrowRef]);
+                
+                return () => ctx.kill();   
+            }
+        })
+    }, []); 
+            
     return (
         <div className={`${styles.container}`}>
             <div className={`${styles.heroTextContainer} w-100`}>
                 <h1 className={styles.heroTextTitle}>Playful Learning Adventures</h1>
                 <h2 className={styles.heroTextSubtitle}>Transform your child's learning experience with Playful Minds!</h2>
                 <p className={styles.heroTextDescription}>Unlock your child's full potential with our <span>fun</span> and  <span>engaging</span> online tutoring program. Our <span>expert tutors</span> provide personalized <span>one-on-one sessions</span> that cater to your child's <span>unique learning style</span>. Sign up today and watch your child's mind soar!</p>
-                <div className={styles.heroBtnContainer}>
-                    <Link href="/register">
-                        <Button 
-                            content="get started"
-                            buttonColor="orangeBg"
-                            buttonTextColor="whiteText"
-                            buttonSize="largeBtn"
-                            buttonFontSize="largeFont"
-                            height="largeHeight"
-    
-                        />
-                    </Link>
+                <div className={styles.arrowContainer}>
+                    <img ref={arrowRef} src="/assets/hero/1.png"></img>
                 </div>
             </div>
             <div className={styles.heroImgContainer}>
