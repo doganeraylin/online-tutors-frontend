afd1ccf344400e5d4d8f9c9a5278549ae8541722
doganeraylin <doganeraylinn@gmail.com>
2023-06-10 17:45:53 +0200

    added counter / routing

--- components/Success/Success.js
+++ components/Success/Success.js
@@ -1,28 +1,43 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import Button from "../Button/Button"
 import styles from "./Success.module.css"
 
 const Success = () => {
+    const [counter, setCounter] = useState(6);
     const router = useRouter();
-    const handleClick = () => {
-        router.push("/");
-    };
-     
+  
+    useEffect(() => {
+      const redirectTimer = setInterval(() => {
+        setCounter((prevCounter) => prevCounter - 1);
+      }, 1000);
+
+      if (counter === 0) {
+        router.push('/');
+        return;
+      }
+      return () => {
+        clearInterval(redirectTimer);
+      };
+    }, [counter, router]);
+      
     return (
         <div className={styles.container}>
-            <p>Thank you for choosing <span>Playful Minds!</span></p>
-            <p>A confirmation email with your booking details has been sent to your email.</p>
-            <p>Contact us if you have any questions or concerns.</p>
-            <p>See you in the next session!</p>
-            <Button 
-                content="back to home"
-                buttonColor="grayBg"
-                buttonTextColor="blackText"
-                buttonSize="mediumBtn"
-                buttonFontSize="mediumFont"
-                height="mediumHeight"
-                onClick={handleClick}
-            />
+           <h1>Welcome to <span>Playful Minds!</span></h1>
+           <p>Your account registration is confirmed, and we can't wait to see where your playful imagination takes you.!</p>
+           <p>Let the adventure begin!</p>
+           <Link href="/">
+                <Button 
+                    content="back to home"
+                    buttonColor="grayBg"
+                    buttonTextColor="blackText"
+                    buttonSize="mediumBtn"
+                    buttonFontSize="mediumFont"
+                    height="mediumHeight"
+                />
+            </Link>
+            <p className={styles.redirectText}>Redirecting to the homepage in <span className={styles.counter}>{counter}</span> seconds...</p>
         </div>
     )
 }
