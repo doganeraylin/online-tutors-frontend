0635fc59e213a2757181980bf9978609598c9538
doganeraylin <doganeraylinn@gmail.com>
2023-06-10 17:43:59 +0200

    added link href

--- components/About/CTA/CTA.js
+++ components/About/CTA/CTA.js
@@ -1,19 +1,22 @@
+import Link from 'next/link'
 import Button from '../../Button/Button'
 import styles from './CTA.module.css'
 
 const CTA = () => {
     return (
         <div className={`${styles.ctaTextContainer} ${styles.lightYellowTitle}`}>
-            <p className={styles.ctaText}>embark on an exciting learning adventure today!</p>
-                <div className={styles.btn}>
-                <Button 
-                    content="sign up"
-                    buttonColor="lightYellowBg"
-                    buttonTextColor="orangeText"
-                    buttonSize="largeBtn"
-                    buttonFontSize="largeFont"
-                    height="largeHeight"
+            <p className={styles.ctaText}>Embark on an exciting learning adventure today!</p>
+            <div className={styles.btn}>
+                <Link href="/register">
+                    <Button 
+                        content="sign up"
+                        buttonColor="lightYellowBg"
+                        buttonTextColor="orangeText"
+                        buttonSize="largeBtn"
+                        buttonFontSize="largeFont"
+                        height="largeHeight"
                     />
+                </Link>
             </div>
         </div>
     )
