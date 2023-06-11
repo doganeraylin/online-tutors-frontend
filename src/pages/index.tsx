3bc6a9af2967ce6140418e2b63d21e315e11db4a
doganeraylin <doganeraylinn@gmail.com>
2023-06-11 14:16:28 +0200

    added all components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import ExploreTutors from '../../components/ExploreTutors/ExploreTutors';
 import MeetTutor from '../../components/MeetTutors/MeetTutors'
-import Reviews from '../../components/Reviews/Reviews';
+import Testimonials from '../../components/Testimonials/Testimonials';
 import Footer from '../../components/Footer/Footer'
 import CTA from '../../components/CTA/CTA'
 import Discount from '../../components/Discount/Discount'
 import Hero from "../../components/Hero/Hero"
 import WhyPlayfulMinds from "../../components/WhyPlayfulMinds/WhyPlayfulMinds"
 import HowItWorks from "../../components/HowItWorks/HowItWorks"
-import Success from "../../components/Success/Success"
 import Layout from "../../components/Layout/Layout"
 import { getTestimonials } from "../../lib/tutors"
-
 
 interface Testimonials {
   testimonials: []
@@ -18,25 +16,24 @@ interface Testimonials {
 
 export async function getStaticProps() { 
   const testimonials = await getTestimonials()
-  console.log(testimonials)
   return {
     props: {
-      testimonials
+      testimonials,
     },
   };
 }
 
-export default function Home ({ testimonials } : Testimonials) {
+export default function Home({ testimonials }: Testimonials ) {
   return (
   <>
-  <Layout bgColor="#A6E1D8">
+  <Layout>
     <Hero />
     <WhyPlayfulMinds />
     <HowItWorks />
-    <Reviews testimonials={testimonials}/>
+    <Testimonials testimonials={testimonials}/>
     <MeetTutor/>
     <ExploreTutors/>
-    {/* <Discount /> */}
+    <Discount />
     <div className="home-page-bg-transition">
       <CTA />
       <Footer /> 
