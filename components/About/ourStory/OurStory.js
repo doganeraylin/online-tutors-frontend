a332a9e990028b98a21033ff10dc5073b6cf9b0b
doganeraylin <doganeraylinn@gmail.com>
2023-06-11 14:10:06 +0200

    removed animation

--- components/About/ourStory/OurStory.js
+++ components/About/ourStory/OurStory.js
@@ -1,44 +1,11 @@
-// import { useRef, useEffect } from 'react';
-// import gsap from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
-// import { matchMedia, mediaQueries } from "../../AnimationScreenSize/animationScreenSize"
 import styles from "./ourStory.module.css"
-import { useRef, useEffect} from "react"
-import { gsap } from 'gsap';
-import { matchMedia, mediaQueries } from "../../AnimationScreenSize/animationScreenSize"
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger);
 
 const ourStory = () => {
-    const titleRef = useRef(null)
-    useEffect(() => {
-        matchMedia.add(mediaQueries, (context) => {
-            let { isDesktop } = context.conditions;
-            if (isDesktop) {
-                let ctx = gsap.context(() => {
-               
-                gsap.from(titleRef.current, {
-                    scrollTrigger: {
-                        trigger: titleRef.current,
-                        toggleClass: {
-                            targets: titleRef.current,
-                            className : styles.titleActive
-                        },
-                        start: "top 90%", 
-                    }
-                })}, [titleRef]);
-            
-                return () => ctx.revert();
-            }
-        })
-    }, []);
-        
     return (
         <>
-        <div  className={`${styles.introContainer} ${styles.lightYellowTitle}`}>
-            <p ref={titleRef} className={styles.titleItalic}>from vision to reality:</p>
-            <p  className={styles.title}>the story of</p>
+        <div className={`${styles.introContainer} ${styles.lightYellowTitle}`}>
+            <p className={styles.titleItalic}>from vision to reality:</p>
+            <p className={styles.title}>the story of</p>
             <p className={styles.titleColored}>Playful Minds!</p>
         </div>
         <div className={`${styles.storyContainer} ${styles.lightYellowText}`}>
