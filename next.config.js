51bf0bba4691eda625b518c6eebe4d6ddfe4c3ec
doganeraylin <doganeraylinn@gmail.com>
2023-06-11 14:19:22 +0200

    added react strict mode

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
 module.exports = {
   webpack: (config) => {
@@ -15,3 +15,4 @@ module.exports = {
     return config;
   },
 };
+
