4a0b2405640978e6ee5691e6116e6b5dd3a4bd9f
doganeraylin <doganeraylinn@gmail.com>
2023-06-11 14:15:42 +0200

    created new contact page

---
+++ src/pages/contact-page.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout/Layout";
+import ContactInfo from "../../components/ContactInfo/ContactInfo"
+
+const contactPage = () => {
+  return (
+    <Layout>
+      <ContactInfo/>
+    </Layout>
+  )
+}
+
+export default contactPage
