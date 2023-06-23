57e6dd3d1de2de3fedd625628d5d62bd39533976
doganeraylin <doganeraylinn@gmail.com>
2023-06-23 14:44:57 +0200

    edited text and img

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -31,8 +31,8 @@ const Hero = () => {
     }, []); 
             
     return (
-        <div className={`${styles.container}`}>
-            <div className={`${styles.heroTextContainer} w-100`}>
+        <div className={styles.container}>
+            <div className={styles.heroTextContainer}>
                 <h1 className={styles.heroTextTitle}>Playful Learning Adventures</h1>
                 <h2 className={styles.heroTextSubtitle}>Transform your child's learning experience with Playful Minds!</h2>
                 <p className={styles.heroTextDescription}>Unlock your child's full potential with our <span>fun</span> and  <span>engaging</span> online tutoring program. Our <span>expert tutors</span> provide personalized <span>one-on-one sessions</span> that cater to your child's <span>unique learning style</span>. Sign up today and watch your child's mind soar!</p>
@@ -41,7 +41,7 @@ const Hero = () => {
                 </div>
             </div>
             <div className={styles.heroImgContainer}>
-                <img className={`${styles.heroImg} w-100 `}src="./assets/hero/hero-img.png"></img>
+                <img className={styles.heroImg} src="./assets/hero/hero-img.png"></img>
             </div>
         </div>  
     )   
